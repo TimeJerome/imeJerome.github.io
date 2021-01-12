f6f1b4c31bf95e86a851f482b7f985f44c47b4e8
TimeJerome <76664411+TimeJerome@users.noreply.github.com>
2021-01-13 00:59:46 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,51 @@
 $(document).ready(function(event) {
     $('.hi__burger').click(function(event) {
-        $('.hi__burger,.menu__menu').toggleClass('active');
+        $('.hi__burger,.menu__menu,.hi__soc,.slick-arrow').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
 $(document).ready(function(event) {
     $('.menu__element._el').click(function(event) {
         $('.menu__list').toggleClass('active');
+    });
+});
+$(document).ready(function(event) {
+    $('.textBack').click(function(event) {
+        $('.textBack,.textBack,.hi__soc,.original').toggleClass('active');
+    });
+});
+$(document).ready(function(event) {
+    $('.slider__body').slick({
+        autoplay:false,
+    });
+
+});
+
+$(document).ready(function(event) {
+    $('.slider__title').slick({
+        autoplay:true,
+        arrows:false,
+        slidesToShow:1,
+    });
+});
+
+$(document).ready(function(event) {
+    $('.geo__title1').click(function(event) {
+        $('.geo__title1,.geo__spoil1').toggleClass('active').next().slideToggle(300);
+    });
+});
+$(document).ready(function(event) {
+    $('.geo__title2').click(function(event) {
+        $('.geo__title2,.geo__spoil2').toggleClass('active').next().slideToggle(300);
+    });
+});
+$(document).ready(function(event) {
+    $('.geo__title3').click(function(event) {
+        $('.geo__title3,.geo__spoil3').toggleClass('active').next().slideToggle(300);
+    });
+});
+$(document).ready(function(event) {
+    $('.geo__title4').click(function(event) {
+        $('.geo__title4,.geo__spoil4').toggleClass('active').next().slideToggle(300);
     });
 });
